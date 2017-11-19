b853e28df7c29fca6c01db02fb6774190b724e90
Kolya154000 <kolya154@gmail.com>
2017-11-19 22:30:21 +0200

    goToHome3

--- Percolation.java
+++ Percolation.java
@@ -1,8 +1,8 @@
 /**
- * Write a description of class Percolation here.
+ * Class Percolation.
  *
  * @author Mykola
- * @version (a version number or a date)
+ * @version 19.11.2017
  */
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
@@ -10,20 +10,19 @@ public class Percolation
 {
    private int side; 
    private char[][] cond; 
-   private int size;
    private int openNumber;
-   private WeightedQuickUnionUF id;
+   private final WeightedQuickUnionUF id;
    
    /**
     * Constructor for objects of class Percolation
     */
    public Percolation(int n) // create n-by-n cond, with all sites blocked
    {
-       if(n <= 0)
+       if (n <= 0)
        throw new IllegalArgumentException("Side of grid must be bigger than 0"); 
        side = n;
-       size = n * n;
-      id = new WeightedQuickUnionUF(size);
+       int size = n * n;
+       id = new WeightedQuickUnionUF(size);
        cond = new char[n][n];
        for(int i = 0; i < n; i++)
        {
@@ -42,7 +41,7 @@ public class Percolation
    
    private boolean validate(int row, int col) // check invalid indicies
    {
-      if(row < 1 || row > side || col < 1 || col > side) {
+      if (row < 1 || row > side || col < 1 || col > side) {
           throw new IllegalArgumentException("row " + row + " or col " + col + " is not between 1 and " + side);
           }  
       else
@@ -52,32 +51,38 @@ public class Percolation
    private void connectNeighbours(int row, int col) // connect open neighbours that are below, above, right or left
    {
        int p = xyTo1D(row, col);
-       if(isOpen(row, Math.max(1, col - 1)))   //left neighbour
+       if (isOpen(row, Math.max(1, col - 1)))   //left neighbour
        {
            int q = xyTo1D(row, Math.max(1, col - 1));
            id.union(p, q);
        }
-       if(isOpen(Math.max(1, row - 1), col)) //above neighbour
+       if (isOpen(Math.max(1, row - 1), col)) //above neighbour
        {
            int q = xyTo1D(Math.max(1, row - 1), col);
            id.union(p, q);
        } 
-       if(isOpen(row, Math.min(side, col + 1)))  // right neighbour
+       if (isOpen(row, Math.min(side, col + 1)))  // right neighbour
        {
            int q = xyTo1D(row, Math.min(side, col + 1));
            id.union(p, q);
        }
-       if(isOpen(Math.min(side, row + 1), col))  // below neighbour
+       if (isOpen(Math.min(side, row + 1), col))  // below neighbour
        {
            int q = xyTo1D(Math.min(side, row + 1), col);
            id.union(p, q);
        }
    }
    
+   /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
    public void open(int row, int col) // open site (row, col) if it is not open already
    {
        validate(row, col);
-       if(!(isOpen(row, col)))
+       if (!(isOpen(row, col)))
        {
            cond[row - 1][col - 1] = 'o';
            connectNeighbours(row, col);
@@ -85,21 +90,33 @@ public class Percolation
        }    
    }
    
+   /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
    public boolean isOpen(int row, int col)  // is site (row, col) open?
    {
        validate(row, col);
-       if(cond[row - 1][col - 1] == 'c')
+       if (cond[row - 1][col - 1] == 'c')
        return false;
        return true;
    }
    
+   /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
    public boolean isFull(int row, int col)  // is site (row, col) full?
    {
        validate(row, col);
        int p = xyTo1D(row, col);
-       for(int i = 0; i < side; i++)
+       for (int i = 0; i < side; i++)
        {
-           if(id.connected(i, p) && isOpen(1, i + 1))
+           if(isOpen(1, i + 1) && id.connected(i, p))
            {
                return true;
            }   
@@ -107,52 +124,30 @@ public class Percolation
        return false;
    }
    
+   /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
    public int numberOfOpenSites()  // number of open sites
    {
        return openNumber;    
    }
    
+   /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
    public boolean percolates()         // does the system percolate?
    {
-       for(int i = 1; i <= side; i++)
+       for (int i = 1; i <= side; i++)
        {
            if(isFull(side, i))
            return true;
        }   
        return false;
    }
-   
-   
-  /* public static void main(String[] args)
-   {
-      int n = 200;         // n-by-n percolation system
-      float sum = 0;
-      int count = 0;
-       Random g = new Random(); 
-       // repeatedly read in sites to open and draw resulting system
-      Percolation perc;
-      for(int i = 0; i < 10; i++)
-      {
-       perc = new Percolation(n);
-      while (!perc.percolates())
-      {
-           int r = g.nextInt(n) + 1;
-           int j = g.nextInt(n) + 1;
-           perc.open(r, j);
-      }
-      System.out.println((float)perc.numberOfOpenSites() / (n * n));
-      sum+= (float)perc.numberOfOpenSites() / (n * n);
-      count++;
-    }
-    System.out.println("mean = " + sum / count); 
-       
-   } // test client (optional)
-
-       /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    
 }
--- PercolationStats.java
+++ PercolationStats.java
@@ -1,29 +1,27 @@
-/**
- * Write a description of class PercolationStats here.
+/**----------------------------------------------------
  * 
- * @author (your name) 
- * @version (a version number or a date)
- */
+ * 
+ * @author Mykola 
+ * @version 19.11.2017
+ *---------------------------------------------------*/
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.StdDraw;
 
 public class PercolationStats {
-   private int n;
    private int t;
    private Percolation perc;
    private double[] fractions;
    
    public PercolationStats(int n, int trials)    // perform trials independent experiments on an n-by-n grid
    {
-       if(n <= 0 || trials <= 0)
+       if (n <= 0 || trials <= 0)
        throw new java.lang.IllegalArgumentException();
-       this.n = n;
        this.t = trials;
+       int size = n * n;
        perc = new Percolation(n);
        fractions = new double[t];
-       //StdDraw.enableDoubleBuffering();
-       for(int i = 0; i < t; i++)
+       for (int i = 0; i < t; i++)
        {
            while (!perc.percolates())
            {
@@ -33,36 +31,66 @@ public class PercolationStats {
                //PercolationVisualizer.draw(perc, n);
               // StdDraw.show();
            }  
-           fractions[i] = (double)perc.numberOfOpenSites() / (n * n);
-           System.out.println(i + " " + fractions[i]);
+           fractions[i] = (double)perc.numberOfOpenSites() / size;
+           System.out.println(i + 1 + " - " + fractions[i]);
            perc = new Percolation(n);
        }
    }
    
+   /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
    public double mean()            // sample mean of percolation threshold
    {
        return StdStats.mean(fractions);     
    }
    
+   /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
    public double stddev()          // sample standard deviation of percolation threshold
    {
        return StdStats.stddev(fractions);    
    }
    
+   /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
    public double confidenceLo()      //low  endpoint of 95% confidence interval
    {
        return mean() - 1.96 * stddev() / Math.sqrt(t);    
    }
    
+   /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
    public double confidenceHi()         // high endpoint of 95% confidence interval
    {
        return mean() + 1.96 * stddev() / Math.sqrt(t);
    }
    
+   /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
    public static void main(String[] args)        // test client (described below)
    {
-       int n = 200; //Integer.parseInt(args[0]); 
-       int trials = 10; // Integer.parseInt(args[1]);
+       int n = 200; //     Integer.parseInt(args[0]); 
+       int trials = 100; // Integer.parseInt(args[1]);
        PercolationStats stats = new PercolationStats(n, trials);
                       
        System.out.println("mean                    = " + stats.mean());
